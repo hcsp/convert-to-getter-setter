8e0145de7e181e2d12fa356dfce32d4c3ff166f0
sizeof <625327545@qq.com>
2019-07-26 14:26:12 +0800

    将原先的public成员改写成getter/setter方法

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -5,6 +5,24 @@ public class Cat {
     public String name;
     /** 猫咪的年龄 */
     public int age;
+    /** 猫咪是否萌，true为萌，false为不萌 */
+    public boolean cute;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public boolean isCute() {
         return cute;
@@ -13,7 +31,4 @@ public class Cat {
     public void setCute(boolean cute) {
         this.cute = cute;
     }
-
-    /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
 }
