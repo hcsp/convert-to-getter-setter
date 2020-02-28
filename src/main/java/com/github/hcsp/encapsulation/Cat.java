8e3cf8f15a61e7848266297d37b1316d74dcdb87
pathjh <1639925487@qq.com>
2020-02-28 12:38:50 +0800

    setter/getter实现封装，降低耦合性

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -3,8 +3,34 @@ package com.github.hcsp.encapsulation;
 public class Cat {
     /** 猫咪的名字 */
     public String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     /** 猫咪的年龄 */
     public int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
     /** 猫咪是否萌，true为萌，false为不萌 */
     public boolean cute;
+
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
+
+    public boolean isCute() {
+        return cute;
+    }
 }
