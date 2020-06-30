3904196c6588c72a8e7952cdf035821e0afa4cc6
admin <1269583342@qq.com>
2020-06-30 14:37:20 +0800

    将成员改写为getter/setter

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -2,9 +2,34 @@ package com.github.hcsp.encapsulation;
 
 public class Cat {
     /** 猫咪的名字 */
-    public String name;
+    private String name;
     /** 猫咪的年龄 */
-    public int age;
+    private int age;
     /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
+    private boolean cute;
+    private Object String;
+
+    public Cat getString() {
+        return (Cat) String;
+    }
+
+    public void setString(Cat string) {
+        String = string;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isCute() {
+        return cute;
+    }
+
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
 }
