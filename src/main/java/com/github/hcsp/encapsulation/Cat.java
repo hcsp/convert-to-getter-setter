b6941bf88ea73296715d0211fbf4711a19b01bd7
jcbound.ma <46434117+jcbound@users.noreply.github.com>
2021-12-11 17:29:51 +0800

    增加get方法，set方法 (#252)
    增加get方法，set方法 (#252)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -2,9 +2,27 @@ package com.github.hcsp.encapsulation;
 
 public class Cat {
     /** 猫咪的名字 */
-    public String name;
+    private String name;
     /** 猫咪的年龄 */
-    public int age;
+    private int age;
     /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
+    private  boolean cute;
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public boolean isCute(){
+        return cute;
+    }
+    public String setName(String name){
+        return this.name=name;
+    }
+    public int setAge(int age){
+        return this.age=age;
+    }
+    public boolean setCute(boolean cute){
+        return this.cute=cute;
+    }
 }
