6d06de2aa8b08a78f4110866d17d32e5ba3efc5e
rico <5924435@qq.com>
2022-02-17 14:45:35 +0800

    改造set get

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -3,6 +3,29 @@ package com.github.hcsp.encapsulation;
 public class Cat {
     /** 猫咪的名字 */
     public String name;
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isCute() {
+        return cute;
+    }
+
     /** 猫咪的年龄 */
     public int age;
     /** 猫咪是否萌，true为萌，false为不萌 */
