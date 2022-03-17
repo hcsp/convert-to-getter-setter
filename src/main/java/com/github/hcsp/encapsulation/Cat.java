98514e7635e28aa7701972fc35dc754fff672d6b
yezhiguang <847786582@qq.com>
2022-03-17 21:35:18 +0800

    fix getterandsetter

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -25,10 +25,16 @@ public class Cat {
         this.cute = cute;
     }
 
-    /** 猫咪的名字 */
-    public String name;
-    /** 猫咪的年龄 */
-    public int age;
-    /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
+    /**
+     * 猫咪的名字
+     */
+    private String name;
+    /**
+     * 猫咪的年龄
+     */
+    private int age;
+    /**
+     * 猫咪是否萌，true为萌，false为不萌
+     */
+    private boolean cute;
 }
