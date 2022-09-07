2f728b12a8b2754561417fb314a58474c32784b6
oxc <524112857@qq.com>
2022-09-07 18:34:23 +0800

    aa

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -12,6 +12,14 @@ public class Cat {
         return age;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void setAge(int age) {
         this.age = age;
     }
