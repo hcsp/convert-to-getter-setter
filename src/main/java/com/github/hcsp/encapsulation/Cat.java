79aaaeaebb14f8dc471c124259628c7a1bbd2781
seagull-z <81363135+seagull-z@users.noreply.github.com>
2023-11-16 21:15:20 +0800

    Update Cat.java

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -2,7 +2,6 @@ package com.github.hcsp.encapsulation;
 
 public class Cat {
     /** 猫咪的名字 */
-<<<<<<< HEAD
     private String name;
     /** 猫咪的年龄 */
     private int age;
@@ -13,25 +12,18 @@ public class Cat {
         this.name=name;
     }
     public String getName(String name){
-        return this.name;
+        return name;
     }
     public void setAge(int age){
         this.age=age;
     }
     public int getAge(int age){
-        return this.age;
+        return age;
     }
     public void setCute(boolean cute){
         this.cute=cute;
     }
     public boolean isCute(boolean cute){
-        return  this.cute;
+        return  cute;
     }
-=======
-    public String name;
-    /** 猫咪的年龄 */
-    public int age;
-    /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
->>>>>>> c47a1c47a034ed8c614984f7381c4261e484867e
 }
